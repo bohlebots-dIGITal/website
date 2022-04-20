c31bdf28aa020f77aecc4bf8cb75c0fe3bb352ef
3nt3 <gott@3nt3.de>
2022-04-20 16:34:47 +0200

    asghkajshgdlasd
    Signed-off-by: 3nt3 <gott@3nt3.de>

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  mode: "jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: [
+        "Inter",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+    },
+    extend: {
+      colors: {
+        primary: "#202225",
+        secondary: "#5865f2",
+        gray: {
+          900: "#202225",
+          800: "#2f3136",
+          700: "#36393f",
+          600: "#4f545c",
+          400: "#d4d7dc",
+          300: "#e3e5e8",
+          200: "#ebedef",
+          100: "#f3f2f5",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
