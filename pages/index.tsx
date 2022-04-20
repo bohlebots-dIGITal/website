1b872615d84191c5ef579ae18242cf848a35b58d
3nt3 <gott@3nt3.de>
2022-04-20 17:09:48 +0200

    add mp4 for helena
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,11 +29,11 @@ const Home: NextPage = () => {
 
   return (
     <div>
-      <video
-        src="/animation.webm"
-        ref={videoRef}
-        className="object-cover -z-10 h-screen w-screen"
-      ></video>
+      <video ref={videoRef} className="object-cover -z-10 h-screen w-screen">
+        <source src="/animation.webm" type="video/webm" />
+        <source src="/animation.mp4" type="video/mp4" />
+        This browser does not support HTML5 video elements.
+      </video>
       <div className="flex justify-center items-center z-10 h-screen absolute left-0 top-0 w-screen flex-col text-center">
         <h1 className="text-6xl text-white font-bold md:text-9xl">dIGITal</h1>
         <p className="text-white text-2xl">At least halfway decent</p>
