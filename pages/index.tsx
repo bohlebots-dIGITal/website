059d1bcb832c1b80830eaf70e013b8872dd9baa4
3nt3 <gott@3nt3.de>
2022-04-21 12:16:17 +0200

    change logo and count up numbers
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from "next";
-import { createRef, useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import { Fragment, useEffect, useState } from "react";
+import CountUp from "react-countup";
+import VisibilitySensor from "react-visibility-sensor";
+import Logo from "../public/logo.png";
 
 const Home: NextPage = () => {
+  const duration = 0.4;
+
+  useEffect(() => {});
+
   return (
     <div>
       <video
@@ -16,25 +24,35 @@ const Home: NextPage = () => {
         This browser does not support HTML5 video elements.
       </video>
       <div className="flex justify-center items-center z-10 h-screen absolute left-0 top-0 w-screen flex-col text-center">
-        <h1 className="text-6xl text-white font-bold md:text-9xl">dIGITal</h1>
+        <div className="relative md:w-[30rem] w-72">
+          <Image src={Logo} className="absolute top-0 left-0" />
+        </div>
         <p className="text-white text-2xl">At least halfway decent</p>
       </div>
-      <section className="w-full bg-black py-20 px-4 md:px-24">
-        <span className="text-white text-6xl font-bold">
-          63 commits.{" "}
-          {Math.floor(
-            (new Date().getTime() - new Date("2021-05-03").getTime()) /
-              1000 /
-              60 /
-              60 /
-              24
-          )}{" "}
-          days. 27 parts. 54 hardware-iterations.{" "}
-          <span className="text-purple-400">
-            One robot that plays f*cking soccer.
-          </span>
-        </span>
-      </section>
+      <VisibilitySensor partialVisibility offset={{ bottom: 100 }}>
+        {({ isVisible }: any) => (
+          <section className="w-full bg-black py-20 px-4 md:px-24">
+            {isVisible ? (
+              <span className="text-white text-6xl font-bold">
+                <CountUp end={63} duration={duration} /> commits.{" "}
+                {Math.floor(
+                  (new Date().getTime() - new Date("2021-05-03").getTime()) /
+                    1000 /
+                    60 /
+                    60 /
+                    24
+                )}{" "}
+                days.
+                <CountUp end={27} duration={duration} /> parts.{" "}
+                <CountUp end={67} duration={duration} /> hardware iterations.{" "}
+                <span className="text-purple-400">
+                  One robot that plays f*cking soccer.
+                </span>
+              </span>
+            ) : null}
+          </section>
+        )}
+      </VisibilitySensor>
     </div>
   );
 };
